3d5565dd2df821fb5222f495fb8e68b7e0410f33
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-04-17 12:08:09 +0530

    Completed

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,23 @@
 // write the book component code here
+import React from 'react'
+
+function BookCard({ book }) {
+    return(
+        <div className="book-card" style={{
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+            padding: '16px',
+            margin: '10px',
+            width: '200px',
+            textAlign: 'center',
+            backgroundColor: '#f9f9f9'
+          }}>
+        <img src={book.image} alt={book.image} style={{ width: '100%', height: '250px', objectFit: 'cover', borderRadius: '6px' }} />
+        <h3>{book.name}</h3>
+        <p>Genre: {book.genre}</p>
+        <p>Author: {book.author}</p>
+        </div>
+    )
+}
+
+export default BookCard;
